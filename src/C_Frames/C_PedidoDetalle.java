73c82c622c466703e01fb2c9da61867ad52831d3
Emilio <Emilio@DESKTOP-G2AOF7N>
2018-11-27 22:00:18 -0600

    LAS MIAS

--- src/C_Frames/C_PedidoDetalle.java
+++ src/C_Frames/C_PedidoDetalle.java
@@ -301,21 +301,10 @@ public void borrarTabla(JTable tab) {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        //System.out.println(tablaPress.getValueAt(tablaPress.getSelectedRow(), 6));
-        if ((tablaPedidoDet.getValueAt(tablaPedidoDet.getSelectedRow(), 6)+"").equals("B")){
-          int ID = 0;
-          try{
-          ID = Integer.parseInt(""+tablaPedidoDet.getValueAt(tablaPedidoDet.getSelectedRow(), 0));
-            if(JOptionPane.showConfirmDialog(null, "¿Desea darlo de alta?","Informacion",
-        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                erp.SQL("update PresentacionesProducto set estatus = 'A' where idPresentacion = "+ID);
-            
-              }else{
-                JOptionPane.showMessageDialog(null,"NO Actualizado","Infromación" ,JOptionPane.INFORMATION_MESSAGE);
-            }
-          }catch(Exception e){
-              JOptionPane.showMessageDialog(null,e.getMessage(),"Error" ,JOptionPane.INFORMATION_MESSAGE);
-          }
-      } 
+      //
+      StringTokenizer b = new StringTokenizer(tablaPedidoDet.getValueAt(tablaPedidoDet.getSelectedRow(), 7)+""," ");
+      int a = Integer.parseInt(b.nextToken());
+      erp.SQL("Update Pedidos set Estatus = '"+cmbSe.getItemAt(cmbSe.getSelectedIndex()).charAt(0)+"' where idPedido = "+a);
         btnConsultar.doClick();
     }//GEN-LAST:event_jButton1ActionPerformed
 
